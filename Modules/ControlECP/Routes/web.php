a646e1377b8a064fc3b1aff03d3b079bcbe86d78
mesolevar1-crypto <mesolevar1@gmail.com>
2026-09-09 17:53:54 -0500

    Revertir inicio de sesión

--- Modules/ControlECP/Routes/web.php
+++ Modules/ControlECP/Routes/web.php
@@ -18,17 +18,7 @@ Route::get('/control-ecp/inicio', [ControlECPController::class, 'index'])->name(
 // --- 2. RUTAS PROTEGIDAS (Gestión interna / CRUD de Elementos) ---
 Route::middleware(['auth', 'verified'])->group(function () {
 
-    // Resource del controlador principal (sin 'index' para evitar colisión con la ruta pública)
-    Route::resource('controlecps', ControlECPController::class)
-        ->except(['index'])
-        ->names([
-            'create'  => 'controlecp.resource.create',
-            'store'   => 'controlecp.resource.store',
-            'show'    => 'controlecp.resource.show',
-            'edit'    => 'controlecp.resource.edit',
-            'update'  => 'controlecp.resource.update',
-            'destroy' => 'controlecp.resource.destroy',
-        ]);
+    Route::resource('controlecps', ControlECPController::class)->names('controlecp');
 
     // Gestión de Elementos
     Route::get('/control-ecp/elementos', [ElementoController::class, 'index'])
