a88846a9ed85048125eb00a898ff9bf74f059c3b
mesolevar1-crypto <mesolevar1@gmail.com>
2026-09-14 11:10:56 -0500

    Actualización del proyecto

--- Modules/ControlECP/Resources/views/components/sidebar_administrador.blade.php
+++ Modules/ControlECP/Resources/views/components/sidebar_administrador.blade.php
@@ -29,17 +29,17 @@
         @php
             $items = [
                 ['icon' => 'fa-house', 'label' => 'Inicio', 'route' => 'controlecp.administrador.inicio'],
-                ['icon' => 'fa-users', 'label' => 'Usuarios', 'route' => 'controlecp.administrador.usuarios'],
-                ['icon' => 'fa-id-card', 'label' => 'Fichas', 'route' => 'controlecp.administrador.fichas'],
-                ['icon' => 'fa-star', 'label' => 'Momentos', 'route' => 'controlecp.administrador.momentos'],
-                ['icon' => 'fa-list-check', 'label' => 'Actividades', 'route' => 'controlecp.administrador.actividades'],
-                ['icon' => 'fa-calendar-days', 'label' => 'Cronograma', 'route' => 'controlecp.administrador.cronograma'],
-                ['icon' => 'fa-user-check', 'label' => 'Asistencia', 'route' => 'controlecp.administrador.asistencia'],
-                ['icon' => 'fa-folder-open', 'label' => 'Repositorio', 'route' => 'controlecp.administrador.repositorio'],
-                ['icon' => 'fa-comments', 'label' => 'Asesorías', 'route' => 'controlecp.administrador.asesorias'],
-                ['icon' => 'fa-certificate', 'label' => 'Certificados', 'route' => 'controlecp.administrador.certificados'],
-                ['icon' => 'fa-boxes-stacked', 'label' => 'Inventario', 'route' => 'controlecp.administrador.inventario'],
-                ['icon' => 'fa-comment-dots', 'label' => 'PQR', 'route' => 'controlecp.administrador.pqr'],
+                ['icon' => 'fa-users', 'label' => 'Usuarios', 'route' => NULL],
+                ['icon' => 'fa-id-card', 'label' => 'Fichas', 'route' => NULL],
+                ['icon' => 'fa-star', 'label' => 'Momentos', 'route' => NULL],
+                ['icon' => 'fa-list-check', 'label' => 'Actividades', 'route' => NULL],
+                ['icon' => 'fa-calendar-days', 'label' => 'Cronograma', 'route' => NULL],
+                ['icon' => 'fa-user-check', 'label' => 'Asistencia', 'route' => NULL],
+                ['icon' => 'fa-folder-open', 'label' => 'Repositorio', 'route' => NULL],
+                ['icon' => 'fa-comments', 'label' => 'Asesorías', 'route' => NULL],
+                ['icon' => 'fa-certificate', 'label' => 'Certificados', 'route' => NULL],
+                ['icon' => 'fa-boxes-stacked', 'label' => 'Inventario', 'route' => NULL],
+                ['icon' => 'fa-comment-dots', 'label' => 'PQR', 'route' => NULL],
             ];
         @endphp
 
